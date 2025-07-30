eeab0d393f8c8617f64271da31bab963609af200
Balakeavan <gddeveloper463@gmail.com>
2025-07-30 11:50:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js', { scope: '/' })
+        navigator.serviceWorker.register('./sw.js', { scope: '/' })
             .then(reg => console.log('[SW] Registered!', reg))
             .catch(err => console.error('[SW] Failed:', err));
     });
